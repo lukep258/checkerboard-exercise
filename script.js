2a890f81bab6373a83d35372dd2d46aabc017f45
Luke Park <143543147+lukep258@users.noreply.github.com>
2023-10-10 14:57:02 -0500

    dsa

---
+++ script.js
@@ -0,0 +1,9 @@
+var board=document.createElement('container');
+board.setAttribute('display','flex');
+board.setAttribute('flexWrap','wrap');
+board.setAttribute('width','800px');
+board.setAttribute('height','800px');
+var blacktile=document.createClass('black-tile');
+var whitetile=document.createClass('white-tile');
+blacktile.setAttribute('12.5%');
+whitetile.setAttribute('12.5%');
