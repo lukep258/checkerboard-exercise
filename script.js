62b76fd00b37fb557796916fdfa4b11e4dc09907
Luke Park <143543147+lukep258@users.noreply.github.com>
2023-10-11 14:45:44 -0500

    checkerboard

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-var board=document.createElement('container');
-board.setAttribute('display','flex');
-board.setAttribute('flexWrap','wrap');
-board.setAttribute('width','800px');
-board.setAttribute('height','800px');
-var blacktile=document.createClass('black-tile');
-var whitetile=document.createClass('white-tile');
-blacktile.setAttribute('12.5%');
-whitetile.setAttribute('12.5%');
-for(var i=0;i<8;i++){
-    var row = document.createElement('chessrow');
+
+// * `display = "flex"`
+// * `flexWrap = "wrap"`
+// * `width = "800px"`
+// * `height = "800px"`
+
+
+// * `width = "12.5%"`
+// * `height = "12.5%"`
+
+
+let body = document.getElementsByTagName("body")[0]
+
+function makeSquare(container,color){
+    const square = document.createElement('div')
+    square.style.width = "12.5%"
+    square.style.height = "12.5%"
+    square.style.backgroundColor = color
+    container.appendChild(square)
+}
+
+function makeContainer(){
+    const container = document.createElement('div')
+    container.style.display = "flex"
+    container.style.flexWrap = "wrap"
+    container.style.width = "800px"
+    container.style.height = "800px"
+    container.style.backgroundColor = "black"
+    container.style.margin = "auto"
+    body.appendChild(container)
+    return(container);
+}
+
+const chessboard = makeContainer()
+for(let i=0;i<8;i++){
+    for(let j=0;j<8;j++){
+        if(i%2===0&&j%2===0){
+            makeSquare(chessboard,"red")
+            //red
+        }
+        else if(i%2===1&&j%2===1){
+            makeSquare(chessboard,"red")
+            //red
+        }
+        else{
+            makeSquare(chessboard,"black")
+            //black
+        }
+    }
 }
